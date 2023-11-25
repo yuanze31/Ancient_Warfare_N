baa9d0f3e1e0513277a63cb26d7bc4aa6c015521
yuanze31 <1538981620@qq.com>
2023-11-25 20:46:10 +0800

    1.0.9.0

---
+++ main/awB/Script_NeteaseModGuAmt7p8/Parts/Player0/Player0PartMeta.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from Meta.ClassMetaManager import sunshine_class_meta
+from Preset.Model import PartBaseMeta
+
+
+@sunshine_class_meta
+class Player0PartMeta(PartBaseMeta):
+	CLASS_NAME = "Player0Part"
+	PROPERTIES = {}
+
